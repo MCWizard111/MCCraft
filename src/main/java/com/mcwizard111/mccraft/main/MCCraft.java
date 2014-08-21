80660e776f96ab1b09d33f8287fe424c22501f87
MCWizard111 <mcwizard111@live.com>
2014-08-21 18:01:45 -0400

    Adding version file

---
+++ src/main/java/com/mcwizard111/mccraft/main/MCCraft.java
@@ -0,0 +1,14 @@
+package com.mcwizard111.mccraft.main;
+
+import java.util.Random;
+
+/**
+ * Created by MCWizard111 on 8/21/2014.
+ */
+public class MCCraft {
+    private static final MCCraft INSTANCE = new MCCraft();
+
+    public static final Random random = new Random();
+    public static final String currentVersion = "1.0";
+
+}
